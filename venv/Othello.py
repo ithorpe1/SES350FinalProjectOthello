121651e9b5451b2896d9b0b3b677c01ec55d8654
ithorpe1 <64228450+ithorpe1@users.noreply.github.com>
2020-04-28 14:00:00 -0700

    Added diagonal directions to changeList for legal move checker

--- venv/Othello.py
+++ venv/Othello.py
@@ -395,7 +395,7 @@ def isLegal(GameBoard,i,j,playerTurn,Player1,Player2):
             southW += 1
             sWest -= 1
 
-    changeList = eastChange + westChange + southChange + northChange
+    changeList = eastChange + westChange + southChange + northChange +northEastChange + northWestChange + southEastChange + southWestChange
     if len(changeList) == 0:
         #Checks if a move is legal and informs the player if it isn't
         easygui.msgbox("This is not a legal move!", title = "Error!")
